5ff1fe435e963b8d9c2d359dd3c8b251ff63ff19
KauhsalVasava <vasavakaushal333@gmail.com>
2022-05-11 19:36:30 +0530

    Bottom navigation with badges and run time permissions handling

--- app/src/main/java/com/lahsuak/apps/mycompose/ui/PermissionStateExt.kt
+++
@@ -1,9 +0,0 @@
-package com.lahsuak.apps.mycompose.ui
-
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
-
-@ExperimentalPermissionsApi
-fun PermissionState.isPermanentlyDenied(): Boolean {
-    return !shouldShowRationale && !hasPermission
-}
--- app/src/main/java/com/lahsuak/apps/mycompose/ui/screen/HomeScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.lahsuak.apps.mycompose.ui.screen
-
-import androidx.compose.runtime.*
-
-@Composable
-fun HomeScreen() {
-}
-
